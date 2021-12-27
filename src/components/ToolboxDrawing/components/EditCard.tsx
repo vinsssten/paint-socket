2ccdcd0a074f99cd278bd34b110bdc1fe7409c39
vinsssten <dan.wall@mail.ru>
2021-12-27 19:24:01 +0700

    Add clear button logic

--- src/components/ToolboxDrawing/components/EditCard.tsx
+++ src/components/ToolboxDrawing/components/EditCard.tsx
@@ -1,13 +1,31 @@
 import React from 'react'
 import stl from '../ToolboxDrawing.scss'
 
-import clearSVG from '../../../../public/icons/Tools/edit_clear.svg'
 import ToolContainer from '../ToolContainer'
+import Tool from '../../../lib/modules/drawingTools/Tool'
+import { useAppSelector } from '../../..'
+
+import clearSVG from '../../../../public/icons/Tools/edit_clear.svg'
 
 const EditCard = () => {
+    const canvas = useAppSelector(store => store.drawing.canvas);
+
+    function canvasClear () {
+        const msg = 'Are you sure you want to clear the canvas, all changes will be lost?'
+        if (canvas) {
+            if (confirm(msg)) {
+                const context = canvas.getContext('2d')?.clearRect(0, 0,
+                    canvas.width, canvas.height)    
+            }
+        }
+    }
+
     return (
         <ToolContainer title='Edit'>
-            <div className={stl.button}>
+            <div 
+                className={stl.button}
+                onClick={canvasClear}
+                >
                 <img className={stl.image} src={clearSVG} width={35} height={35} />
             </div>
         </ToolContainer>
