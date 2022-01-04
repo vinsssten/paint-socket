c74e44bf57bb57d844bddc102944ac0fb6504468
vinsssten <dan.wall@mail.ru>
2022-01-04 22:43:41 +0700

    Some functionality to auth, Add necessary npm packages

--- server/index.ts
+++ server/index.ts
@@ -9,7 +9,23 @@ const fallback = require('express-history-api-fallback')
 const root = path.join(__dirname, '../') + 'build';
 
 app.use(express.static(root));
-app.use(fallback('index.html', { root }), (req, res, next) => next())
+
+app.get('/api/authorization/', (req, res) => {
+    res.cookie('refresh', '123123123123', {httpOnly: true});
+    res.send();
+})
+
+app.get('/api/authentication', (req, res) => {
+    res.send('Authentication')
+})
+
+app.get('*', (req, res) => {
+    res.sendFile('index.html', {root: root}, err => {
+        if (err) {
+            console.log('error send html', err)
+        }
+    })
+})
 
 app.listen(port, () => {
     console.log('Server started at http://localhost:8080')
