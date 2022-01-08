f5c4ea0dae03dfc46288ecf0933f94c954439095
vinsssten <dan.wall@mail.ru>
2022-01-08 18:14:57 +0700

    Add main page route

--- src/Router.tsx
+++ src/Router.tsx
@@ -5,6 +5,7 @@ import LoginPage from './components/LoginPage/LoginPage';
 import SingleDrawingPage from './components/LocalDrawingPage/SingleDrawingPage';
 import store from './store/store';
 import RedirectPage from './components/RedirectPage/RedirectPage';
+import MainPage from './components/MainPage/MainPage';
 
 function Router() {
     return (
@@ -14,6 +15,7 @@ function Router() {
                     <Route path="/" element={<RedirectPage />} />
                     <Route path="/signin" element={<LoginPage />} />
                     <Route path="/singledrawing" element={<SingleDrawingPage />} />
+                    <Route path="/mainpage" element={<MainPage />} />
                 </Routes>
             </BrowserRouter>
         </Provider>
---
+++ src/components/MainPage/MainPage.tsx
@@ -0,0 +1,14 @@
+import React, { FC } from 'react'
+import stl from './MainPage.scss'
+
+interface Props {
+
+}
+
+const MainPage: FC<Props> = ({  }) => {
+    return (
+        <h1>MAIN PAGE</h1>
+    )
+}
+
+export default MainPage
